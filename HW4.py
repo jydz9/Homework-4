9ebba04706ca1ea1648195414a502a680f24ac07
Jim Yang <76235469+jydz9@users.noreply.github.com>
2022-10-23 01:30:53 -0500

    update code with minimax function

--- HW4.py
+++ HW4.py
@@ -1,4 +1,7 @@
 import copy
+import math
+import time
+from turtle import position
 
 def heuristic(board,player):
     heur = 0
@@ -530,11 +533,125 @@ def legal_moves(board,player):
  
     return legal_moves
 
-def minimax2ply():
-    None
+def find_terminalNodes(board, player):
+    condition = len(legal_moves(board,player)) == 0
+    if(condition):
+        return True
 
-def minimax4ply():
-    None
+def place_piece(board, player, position):
+    copyBoard = copy.deepcopy(board)
+    print(position)
+    copyBoard[position[0]][position[1]] = player
+
+    for r in copyBoard:
+        for c in r:
+            print(c,end = " ")
+        print()
+    return copyBoard
+
+def minimax2ply(board, player, depth, max_min_player):
+    copyBoard = copy.deepcopy(board)
+    terminal_found = find_terminalNodes(board,player)
+    list_moves =  legal_moves(board,player)
+    totalNodes = 0
+    
+    #Terminal node is when player X or O wins, or the board is full
+    if depth == 0 or terminal_found:
+        if terminal_found == True:
+            print("Game is Over")
+        elif(heuristic(board, player) == 1000 or heuristic(board, player) == -1000):
+            return 10000
+        # The depth is 0, return the evaluation of the node
+        else:
+            return heuristic(board, player)
+
+    if max_min_player:
+        score = -math.inf
+        new_score = 0
+        maxNum = 0
+        position = []
+
+        for moves in (list_moves):
+            print(moves[1])
+            if(moves[1] > maxNum):
+                maxNum = moves[1]
+                position = moves[0]
+                print(position)
+        newBoard = place_piece(copyBoard,player,position)
+        # Set to false to switch to min
+        new_score = max(maxNum, minimax2ply(newBoard,player, depth-1, False))
+        print(maxNum, " is the max", position)
+        return new_score
+
+    # Min player
+    else:
+        score = -math.inf
+        new_score = 0
+        minNum = math.inf
+        position = []
+
+        for moves in (list_moves):
+            # print(moves[1])
+            if(moves[1] < minNum):
+                minNum = moves[1]
+                position = moves[0]
+                
+        newBoard2 = place_piece(copyBoard,player,position)
+        # Set to True to switch to max
+        new_score = min(minNum, minimax2ply(newBoard2,player, depth-1, True))
+        return new_score
+
+def minimax4ply(board, player, depth, max_min_player):
+    copyBoard = copy.deepcopy(board)
+    terminal_found = find_terminalNodes(board,player)
+    list_moves =  legal_moves(board,player)
+    totalNodes = 0
+    
+    #Terminal node is when player X or O wins, or the board is full
+    if depth == 0 or terminal_found:
+        if terminal_found == True:
+            print("Game is Over")
+        # The depth is 0, return the evaluation of the node
+        else:
+            return heuristic(board, player)
+
+    if max_min_player:
+        score = -math.inf
+        new_score = 0
+        # For each move we can take
+        maxNum = 0
+        position = []
+
+        for moves in (list_moves):
+            print(moves[1])
+            if(moves[1] > maxNum):
+                maxNum = moves[1]
+                position = moves[0]
+
+        newBoard = place_piece(copyBoard,player,position)
+        # Set to false to switch to min
+        new_score = max(minimax2ply(newBoard,player, depth-1, False))
+        print(maxNum, " is the max", position)
+        return new_score
+
+    # Min player
+    else:
+        score = -math.inf
+        new_score = 0
+        # For each move we can take
+        minNum = math.inf
+        position = []
+
+        for moves in (list_moves):
+            print(moves[1])
+            if(moves[1] < minNum):
+                minNum = moves[1]
+                position = moves[0]
+                
+        newBoard2 = place_piece(copyBoard,player,position)
+        # Set to false to switch to min
+        new_score = min(minimax2ply(newBoard2,player, depth-1, False))
+        return new_score
 
 if __name__ == "__main__":
     # N is for None, no x or o place on that location
@@ -566,3 +683,29 @@ if __name__ == "__main__":
     print(heuristic(board1,"X"))
 
     print(legal_moves(board1,"X"))
+
+    winner = False
+    playerTurn = 'X'
+    start = time.time()
+    findWinner = ""
+    # testing
+    minimax2ply(board1,playerTurn,2,True)
+    # while not winner:
+    #     if(playerTurn == 'p2'):
+    #       playerTurn = 'p1'
+    #       move = minimax2ply(board1,playerTurn,2,True)
+    #       win = heuristic(board, playerTurn)
+    #       if(win == 1000):
+    #           winner = True
+    #           findWinner = "player 2"
+    #     else:
+    #       playerTurn = 'p2' 
+    #       move = minimax2ply(board1,playerTurn,2,True)
+    #       win = heuristic(board, playerTurn)
+    #       if(win == 1000):
+    #           winner = True
+    #           findWinner = "player 1"
+
+    end = time.time()
+    totalTime = end - start
+    print(totalTime)
